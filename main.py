4ed3f5ece199c68a99a454036eff817becf4f7fd
esteban2pac <90542009+esteban2pac@users.noreply.github.com>
2022-03-30 16:00:10 -0500

    Funcionalidad al 70%

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
-from pickle import TRUE
 import random
-from time import strftime
-from classes import alphabet
-from classes import language
+from classes import Alphabet
+from classes import Language
 
 
-
-def unionAlf(alp1, alp2):
+def unionAlp(alp1, alp2):
     list1 = mainList[alp1]
     list2 = mainList[alp2]
     unionList = []
+    if list1.getChainAlphabet() == ["λ"]:
+        return list2.getChainAlphabet()
+    if list2.getChainAlphabet() == ["λ"]:
+        return list1.getChainAlphabet()
+    if list1.getChainAlphabet() == ["λ"] and list2.getChainAlphabet() == ["λ"]:
+        return ["λ"]
     for element in list1.getChainAlphabet():
         if element not in unionList:
             unionList.append(element)
@@ -19,6 +22,24 @@ def unionAlf(alp1, alp2):
             unionList.append(element)
     return unionList
 
+
+def unionLang(lan1,lan2):
+    list1 = languageList[lan1]
+    list2 = languageList[lan2]
+    unionLanList = []
+    if list1.getChainLanguage() == ["λ"]:
+        return list2.getChainLanguage()
+    if list2.getChainLanguage() == ["λ"]:
+        return list1.getChainLanguage()
+    if list1.getChainLanguage() == ["λ"] and list2.getChainLanguage() == ["λ"]:
+        return ["λ"]
+    for element in list1.getChainLanguage():
+        if element not in unionLanList:
+            unionLanList.append(element)
+    for element in list2.getChainLanguage():
+        if element not in unionLanList:
+            unionLanList.append(element)
+    return unionLanList
 
 def alphabetDifference(alp1, alp2):
     list1 = mainList[alp1]
@@ -31,6 +52,17 @@ def alphabetDifference(alp1, alp2):
         return "λ"
     return differenceList
 
+
+def LanguageDifference(lan1,lan2):
+    list1 = languageList[lan1]
+    list2 = languageList[lan2]
+    differenceLanList = []
+    for item in list1.getChainLanguage():
+        if item not in list2.getChainLanguage():
+            differenceLanList.append(item)
+    if differenceLanList == []:
+        return "λ"
+    return differenceLanList
 
 def alphabetIntersection(alp1, alp2):
     list1 = mainList[alp1]
@@ -47,26 +79,38 @@ def alphabetIntersection(alp1, alp2):
 
 def alphabetLock(list, noWords):
     listLocked = ["λ"]
-    for i in range(noWords):
+    for item1 in range(noWords):
         word = ""
-        for j in range(random.randint(2, 5)):
+        for item2 in range(random.randint(2, 5)):
             word += str(list[random.randint(0, len(list) - 1)])
             if word not in listLocked and len(listLocked) <= noWords:
                 listLocked.append(word)
     return listLocked
 
 
+def intersectionLanguage(lan1, lan2):
+    list1 = languageList[lan1]
+    list2 = languageList[lan2]
+    intersectionList = []
+    for item1 in list1.getChainLanguage():
+        for item2 in list2.getChainLanguage():
+            if item1 in list2.getChainLanguage() and item2 in list1.getChainLanguage():
+                intersectionList.append(item1)
+    if intersectionList == []:
+        return "λ"
+    return intersectionList
+
+
 def generateLanguage(lista,noWordsLeng):
-    languageList=[]
-    for i in range(noWordsLeng):
+    languageListAux=[]
+    for item1 in range(noWordsLeng):
         word = ""
-        for j in range(random.randint(1, 55)):
+        for item2 in range(random.randint(1, 55)):
             word += str(lista[random.randint(0, len(lista) - 1)])
-            if word not in languageList and len(languageList) < noWordsLeng:
-                languageList.append(word)
-    print(languageList)
-    languageList.append(languageList)
-    return languageList
+            if word not in languageListAux and len(languageListAux) < noWordsLeng:
+                languageListAux.append(word)
+    print(languageListAux)
+    return languageListAux
 
 
 
@@ -84,7 +128,10 @@ def alpmenu():
     *    4 - Intersección de alfabetos                       *
     *    5 - Cerradura de estrella de alfabeto               *
     *    6 - Generar lenguaje                                *
-    
+    *       Opciones de lenguaje(deben crearse antes)        *
+    *    7 - Union de lenguajes                              *
+    *    8 - Diferencia de lenguajes                         *
+    *    9 - Interseccion de leguajes                        *
     *    0 - Salir                                           *
     **********************************************************
     Opción: """))
@@ -100,7 +147,7 @@ def alpmenu():
                 index2 = int(input("Ingrese el segundo alfabeto que quiere unir: "))
                 if index1 <= alpQuantity and index2 <= alpQuantity:
                     print ("La union de los alfabetos "+str(index1)+" y "+str(index2)+" es: ")
-                    print(unionAlf(index1-1, index2-1))
+                    print(unionAlp(index1-1, index2-1))
                     os.system("PAUSE")
             case 3:
                 index1 = int(input("Ingrese el primer alfabeto:"))
@@ -127,15 +174,35 @@ def alpmenu():
                     print(alphabetLock(mainList[index-1].getChainAlphabet(), quantity))
                     os.system("PAUSE")
             case 6:
+
                 index = int(input("Ingrese el alfabeto sobre el que quiere generear el lenguaje: "))
                 quantity = int(input("Ingrese la cantidad de palabras que desea generar en el lenguaje: "))
                 if index <= alpQuantity:
-                    langObejct = language(generateLanguage(
-                        mainList[index - 1].getChainAlphabet(), quantity))
+                    langObejct = Language(generateLanguage(mainList[index - 1].getChainAlphabet(), quantity))
                     languageList.append(langObejct)
+                    contLang=len(languageList)
                     os.system("PAUSE")
             case 7:
-
+                index1 = int(input("Ingrese el primer lenguaje que quiere unir: "))
+                index2 = int(input("Ingrese el segundo lenguaje que quiere unir: "))
+                if index1 <= contLang and index2 <= contLang:
+                    print("La union de los lenguajes " + str(index1) + " y " + str(index2) + " es: ")
+                    print(unionLang(index1 - 1, index2 - 1))
+                    os.system("PAUSE")
+            case 8:
+                index1 = int(input("Ingrese el primer lenguaje:"))
+                index2 = int(input("Ingrese el lenguaje que quiere restar: "))
+                if index1 <= contLang and index2 <= contLang:
+                    print ("La Diferencia de los lenguajes "+str(index1)+" y "+str(index2)+" es: ")
+                    print(LanguageDifference(index1-1, index2-1))
+                    os.system("PAUSE")
+            case 9:
+                index1 = int(input("Ingrese el primer lenguaje:"))
+                index2 = int(input("Ingrese el lenguaje que quiere intersectar: "))
+                if index1 <= contLang and index2 <= contLang:
+                    print("La interseccion de los lenguajes " + str(index1) + " y " + str(index2) + " es: ")
+                    print(intersectionLanguage(index1 - 1, index2 - 1))
+                    os.system("PAUSE")
             case 0:
                 print("Programa cerrado con exito!")
                 closeProgram = True
@@ -150,7 +217,7 @@ if __name__ == "__main__":
     flag = 0
     mainList = []
     languageList = []
-
+    contLang = 0
     while flag == 0:
         alpQuantity = int(input("Ingrese la cantidad de alfabetos que va a ingresar: "))
         while alpQuantity < 1:
@@ -160,9 +227,9 @@ if __name__ == "__main__":
             alpCad = ""
             alpCad = input("ingrese su alfabeto número " + str(i + 1) + " separada por espacios: ").split(" ")
             if alpCad.__contains__(""):
-                mainList.append(alphabet("λ"))
+                mainList.append(Alphabet("λ"))
             else:
-                alpObject = alphabet(alpCad)
+                alpObject = Alphabet(alpCad)
                 mainList.append(alpObject)
         flag = 1
         alpmenu()
